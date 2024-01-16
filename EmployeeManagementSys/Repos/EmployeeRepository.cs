9c950c503a64a9128a51a3859d471bcc59bdc382
Xhana Ahmetaj <v-xahmetaj@microsoft.com>
2024-01-16 23:54:57 +0100

    added repository layer for Employee

---
+++ EmployeeManagementSys/Repos/EmployeeRepository.cs
@@ -0,0 +1,32 @@
+﻿using EmployeeManagementSys.Models;
+
+namespace EmployeeManagementSys.Repos
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        public Task<bool> CreateEmployee(Employee user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteEmployee(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Employee>> GetEmployee(string adminId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> GetEmployeeById(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateEmployee(Employee user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EmployeeManagementSys/Repos/IEmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using EmployeeManagementSys.Models;
+
+namespace EmployeeManagementSys.Repos
+{
+    public interface IEmployeeRepository
+    {
+        Task<List<Employee>> GetEmployee(string adminId); //return data for all the employees in the database
+
+        Task<Employee> GetEmployeeById(string userId); //return data for one employee based on Id
+
+        Task<bool> DeleteEmployee(string userId);
+        Task<bool> UpdateEmployee(Employee user);
+
+        Task<bool> CreateEmployee(Employee user);
+    }
+}
