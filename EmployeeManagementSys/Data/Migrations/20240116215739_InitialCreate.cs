35c63096f33d83a12f333e843b4851a98800ad2f
Xhana Ahmetaj <v-xahmetaj@microsoft.com>
2024-01-16 23:22:38 +0100

    Add project files.

---
+++ EmployeeManagementSys/Data/Migrations/20240116215739_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeManagementSys.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    ID = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Position = table.Column<int>(type: "int", nullable: false),
+                    Salary = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ EmployeeManagementSys/Models/Employee.cs
@@ -0,0 +1,35 @@
+﻿namespace EmployeeManagementSys.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string ID { get; set; }
+        public Position Position { get; set; }
+        public double Salary { get; set; }
+
+    }
+
+    public enum Position
+    {
+        CTO,
+        CIO,
+        CEO,
+        ProjectManager,
+        ProductManager,
+        Manager,
+        Developer,
+        Tester,
+        Designer,
+        DatabaseAdministrator,
+        SystemAdministrator,
+        DevOpsEngineer,
+        SecurityEngineer,
+        TechnicalWriter,
+        HelpdeskSpecialist,
+        AgileCoach,
+        Analyst,
+        SalesandMarketingProfessionals,
+        HR,
+        FinanceandAccountingProfessionals
+    }
+}
