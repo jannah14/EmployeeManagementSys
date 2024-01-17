c3a5eb45dfb723130e2c4c58716651b8b44b5639
Xhana Ahmetaj <v-xahmetaj@microsoft.com>
2024-01-17 02:00:53 +0100

    mofication to the EmployeDTO

--- EmployeeManagementSys/DTOs/EmployeeDTO.cs
+++ EmployeeManagementSys/DTOs/EmployeeDTO.cs
@@ -4,7 +4,7 @@ namespace EmployeeManagementSys.DTOs
 {
     public class EmployeeDTO
     {
-        internal string? Id { get; set; }
+        public string? Id { get; set; }
         public string Name { get; set; }
         public Position Position { get; set; }
         public double Salary { get; set; }
